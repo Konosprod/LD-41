113e48878f6d71faaf2a1e84421cfc6d18cde3aa
Konosprod <konosprod@free.fr>
2018-04-21 09:47:55 +0200

    Add main menu, options management, add main chara and its animations
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ LD-41/Assets/Scripts/GameSettings.cs
@@ -0,0 +1,6 @@
+﻿public class GameSettings
+{
+    public bool Fullscreen;
+    public float Volume;
+    public int Resolution;
+}
---
+++ LD-41/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioMixer masterMixer;
+
+    public static SoundManager _instance;
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void SetVolume(float newVolume)
+    {
+        masterMixer.SetFloat("Volume", newVolume);
+    }
+
+    public void MuteVolume()
+    {
+        masterMixer.SetFloat("Volume", -80f);
+    }
+}
